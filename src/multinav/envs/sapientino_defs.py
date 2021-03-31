5e74b6ca8e691811fa4169c7638b5c200922dc4c
Roberto Cipollone <cipollone.rt@gmail.com>
2021-03-31 11:26:58 +0200

    Saving map used for experiments on continuous.

--- src/multinav/envs/sapientino_defs.py
+++ src/multinav/envs/sapientino_defs.py
@@ -38,15 +38,12 @@ sapientino_color_sequence = ["red", "green", "blue"]
 
 # This is the shape and configuration of the sapientino map
 sapientino_map_str = """\
-|B p##############|
-|## ##############|
-|## ##############|
-|                 |
-|                 |
-|  b              |
-|## ##########r###|
-|##y########## ###|
-|## ##########g###|"""
+|B p#######|
+|## #######|
+|  b       |
+|## ###r###|
+|##y### ###|
+|## ###g###|"""
 sapientino_n_rooms = 3
 
 
